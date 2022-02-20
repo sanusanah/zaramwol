51df19a41aeeeeb9aabafed07c3f38d836fb03f5
sanusanah <98869034+sanusanah@users.noreply.github.com>
2022-02-20 13:00:22 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -20,8 +20,8 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━✨️Ameer Suhail✨️━━━━━⛦
 
 
-𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 Ameer Suhail 𝐛𝐨𝐭.
-            *Pikachu*
+𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 zara mwol 𝐛𝐨𝐭.
+            *sanu sanah*
 
 ■□■□■□■□■□■□■□■□■□■□
 ♕ *𝙈𝙚𝙙𝙞𝙖 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♕
@@ -134,7 +134,7 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
 🍙𝙐𝙨𝙚➜ Converts text into glowing sticker.
 ⚠️ex  attp pikachu
 ■□■□■□■□■□■□■□■□■□■□
-═════💢Pikachu💢═════
+═════💢zaramwol💢═════
 ▣▣▣▣▣▣▣▣▣ameersuhail▣▣▣▣▣▣▣▣▣▣▣
 
 `}) 
