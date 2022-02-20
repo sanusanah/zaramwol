61caaeb4838b92bfbc5767fb317f98251a3b6953
sanusanah <98869034+sanusanah@users.noreply.github.com>
2022-02-20 20:06:24 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -164,7 +164,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
                     })
                 }
                 else {
-                    await conn.sendMessage(conn.user.jid, '*Bot Started*', MessageType.text);
+                    await conn.sendMessage(conn.user.jid, '*zarabot Started*', MessageType.text);
                 }
             }
             else {
