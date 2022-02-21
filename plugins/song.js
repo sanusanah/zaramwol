91b2965581a276b304924c22a1fc26277df2610e
Sanu <98869034+sanusanah@users.noreply.github.com>
2022-02-21 16:36:30 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -7,15 +7,15 @@ Asena.addCommand({pattern: 'song', fromMe: false,dontAddCommandList: true}, (asy
 
     var r_text = new Array ();    
 r_text[0] = "\nEda eda, mandankonappi.. Nee enne kooduthal mandan aakkatheda\n";
-r_text[1] = "Pikachune tholpikkaan ningalkavilla. Jeevitathil pinkiye tholpichittundu… palarum… palavattom…😥😥 aarellum aaa sed bgm onn iduo...";
-r_text[2] = "Pikachu says i love you 😘";
-r_text[3] = "Pikchude yudhangal Company Kanan Povunnatheyuloo";
+r_text[1] = "e tholpikkaan ningalkavilla. Jeevitathil pinkiye tholpichittundu… palarum… palavattom…😥😥 aarellum aaa sed bgm onn iduo...";
+r_text[2] = "zara says i love you 😘";
+r_text[3] = "zara yudhangal Company Kanan Povunnatheyuloo";
 r_text[4] = "hi, how are you";
 r_text[5] = "ennikk korch rest theroo...🥵🥵";
-r_text[6] = "Pikachu - eva - simsim \ningane ethra ethra paerugal aan alle enikk..😂🤣";
+r_text[6] = "zara - eva - simsim \ningane ethra ethra paerugal aan alle enikk..😂🤣";
 r_text[7] = "താനാരാണെന്ന് തനിക്ക് അറിയാന്‍ മേലങ്കില്‍താന്‍ എന്നോട്‌ ചോദിക്ക്.. താനാരണെന്ന്...തനിക്ക് ഞാന്‍ പറഞ്ഞു തരാംതാനാരാണെന്ന്...എന്നിട്ട്‌ഞാനാരാണെന്ന് എനിക്കാറിയാമൊന്നുതാനെന്നോട്‌ ചോദിക്ക്..അപ്പോ തനിക്ക് ഞാന്‍ പറഞ്ഞു തരാംതാനാരാണെന്നും ..ഞാനാരാണെന്നും - കുതിരവട്ടം പപ്പു, മണിച്ചിത്രതാഴ് ";
 r_text[8] = "\n enne vilicho...?🧐 \n";
-r_text[9] = "ente per Pikachu,ninte per donkey, backi ellarum monkey😼😼";
+r_text[9] = "ente per zara,ninte per donkey, backi ellarum monkey😼😼";
 r_text[10] = "nee vilicha enikk villi kelkathe irrikkaan pattuo";
 r_text[11] = "do you know who is my creator..? its none other than saidali";
 r_text[12] = "രണ്ട് ബക്കറ്റ് നിറയെ വെള്ളമുണ്ട്. അതിൽ ഒരു ബക്കറ്റിനു ദ്വാരമുള്ളതാണ്. എന്നാൽ ദ്വാരമുള്ള ബക്കറ്റിൽ നിന്നും വെള്ളം പോകുന്നില്ല. കാരണം എന്താണ്? para ninak vivaram indo nokkatte";
